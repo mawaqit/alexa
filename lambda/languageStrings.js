115a770835c5a789c52c1c54e98c31b3d4bb32d0
ijaz26 <ijazahmed619@gmail.com>
2024-09-17 20:13:30 +0530

    Added Language Strings

--- lambda/languageStrings.js
+++ lambda/languageStrings.js
@@ -1,9 +1,31 @@
 const en_IN = require("./prompts/en_IN");
-const en_US = require("./prompts/en_US")
-const fr_FR = require("./prompts/fr_FR")
+const en_US = require("./prompts/en_US");
+const fr_FR = require("./prompts/fr_FR");
+const ar_SA = require("./prompts/ar_SA");
+const de_DE = require("./prompts/de_DE");
+const en_AU = require("./prompts/en_AU");
+const en_CA = require("./prompts/en_CA");
+const en_GB = require("./prompts/en_GB");
+const es_ES = require("./prompts/es_ES");
+const es_US = require("./prompts/es_US");
+const it_IT = require("./prompts/it_IT");
+const fr_CA = require("./prompts/fr_CA");
+const nl_NL = require("./prompts/nl_NL");
+const pt_BR = require("./prompts/pt_BR");
 
 module.exports ={
   'en-US': en_US,
   'en-IN': en_IN,
-  'fr-FR': fr_FR
+  'fr-FR': fr_FR,
+  'ar-SA': ar_SA,
+  'de-DE': de_DE,
+  'en-AU': en_AU,
+  'en-CA': en_CA,
+  'en-GB': en_GB,
+  'es-ES': es_ES,
+  'es-US': es_US,
+  'it-IT': it_IT,
+  'fr-CA': fr_CA,
+  'nl-NL': nl_NL,
+  'pt-BR': pt_BR
 }
